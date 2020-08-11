8bbfe546e494be84c8908418345e4618c999f5a3
Eric Denovellis <edeno@bu.edu>
2020-08-10 17:17:23 -0700

    Fix function name

--- trajectory_analysis_tools/__init__.py
+++ trajectory_analysis_tools/__init__.py
@@ -2,6 +2,6 @@
 from trajectory_analysis_tools.distance import (get_distance_metrics,
                                                 get_trajectory_data)
 from trajectory_analysis_tools.highest_posterior_density import (
-    get_highest_posterior_density_threshold, get_HPD_spatial_coverage)
+    get_highest_posterior_threshold, get_HPD_spatial_coverage)
 from trajectory_analysis_tools.posterior import (maximum_a_posteriori_estimate,
                                                  sample_posterior)
