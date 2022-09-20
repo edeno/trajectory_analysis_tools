d7a109c530260af2f37dcb8cec4fca3603e3d64b
Eric Denovellis <edeno@bu.edu>
2022-09-19 22:18:25 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
     name='trajectory_analysis_tools',
-    version='0.4.3.dev0',
+    version='1.0.0',
     license='MIT',
     description=(''),
     author='Eric Denovellis',
